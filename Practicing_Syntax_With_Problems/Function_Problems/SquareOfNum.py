ca75387cf5208b634dc733dfe796543a8d0490cc
Muhammad Talal Zahid <talalzahid912@gmail.com>
2025-10-14 21:27:23 +0500

    function practice problems...

---
+++ Practicing_Syntax_With_Problems/Function_Problems/SquareOfNum.py
@@ -0,0 +1,7 @@
+def Square(n):
+    return n ** 2
+
+
+num = int(input("enter number: "))
+result = Square(num)
+print(f"Square of {num} is {result}")
---
+++ Practicing_Syntax_With_Problems/Function_Problems/SumOfNums.py
@@ -0,0 +1,8 @@
+def Sum(n, m):
+    return n+m
+
+num1 = int(input("enter 1st number: "))
+num2 = int(input("enter 2nd number: "))
+
+result = Sum(num1, num2)
+print(f"Sum is {result}")
