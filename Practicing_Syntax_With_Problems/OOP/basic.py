9609b8905e63f91287d0e11a314414349ad34f19
Muhammad Talal Zahid <talalzahid912@gmail.com>
2025-10-15 19:30:10 +0500

    inheritance...

--- Practicing_Syntax_With_Problems/OOP/basic.py
+++ Practicing_Syntax_With_Problems/OOP/basic.py
@@ -10,9 +10,19 @@ class Car:
         return f"brand: {self.brand} and model: {self.model}"
 
 
+# Problem: Create an ElectricCar class that inherits from the Car class 
+# and has an additional attribute battery_size. 
+class ElectricCar(Car):
+    def __init__(self, brand, model, battery_size):
+        super().__init__(brand, model)
+        self.battery_size = battery_size
+
 
 myCar = Car("honda", "civic")
 print(myCar.brand)
 print(myCar.model)
 
 print(myCar.GetFullName())
+
+electicCar = ElectricCar("tesla", "model s", "100Kwh")
+print(electicCar.GetFullName())
